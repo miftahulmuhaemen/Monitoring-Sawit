92f5dd65ec90aa846672ef0706ed96b88ebd8f60
miftahulmuhaemen <miftahulmuhaemen@gmail.com>
2018-11-12 14:14:47 +0800

    n

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    cors = require('cors'),
-    path = require('path'),
-    bodyParser = require('body-parser');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+const app = express();
 
-app.use(express.static('./dist/your_app_name'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname,'/dist/ng7-pre/index.html'));
+
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
-var routes = require('./routes');
-routes(app);
-app.listen(port);
-console.log("WEB API Running.......");
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
